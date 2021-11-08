a7fe426f5f0c3b98eeffb0b215a2f25f02d6d132
Cristóbal O. Valiente <cr1val@pm.me>
2021-11-08 20:08:32 +0000

    Modificacion de la url de heroes a heros

--- apihero/urls.py
+++ apihero/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'heroes',views.HeroViewSet)
+router.register(r'heros',views.HeroViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
